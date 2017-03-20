b4fd6ce42c9c8a7874484fa5e2f58df20a27cda5
gardncl <gardncl@gmail.com>
2017-03-19 21:24:08 -0400

    Updated readme and global variables for easy testing

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Functional Programming in Javascript
 
-All solutions to the online programming [challange].
+All solutions to the online programming [challange] for learning Microsoft's Reactive Extensions (Rx) using the IDE [brackets].
 
+[brackets]:http://brackets.io/
 [challange]:http://reactivex.io/learnrx/
--- exercises.js
+++ exercises.js
@@ -1,8 +1,15 @@
-var one = function() {
-	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
-		counter;
+var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
 
+var one = function() {
+	var	counter;
+    
 	for(counter = 0; counter < names.length; counter++) {
 		console.log(names[counter]);
 	}
+}
+
+var two = function() {
+	names.forEach(function(name) {
+		console.log(name);
+	});
 }
