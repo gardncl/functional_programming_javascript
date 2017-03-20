ea90a0c9f9097504d240c0aa1f2a3dc945abc6bd
gardncl <gardncl@gmail.com>
2017-03-19 21:18:30 -0400

    First exercise and setup for brackets

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# functional_programming_javascript
+# Functional Programming in Javascript
+
+All solutions to the online programming [challange].
+
+[challange]:http://reactivex.io/learnrx/
---
+++ exercises.js
@@ -0,0 +1,8 @@
+var one = function() {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+		counter;
+
+	for(counter = 0; counter < names.length; counter++) {
+		console.log(names[counter]);
+	}
+}
